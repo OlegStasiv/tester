a8ae2dfdb385771b54f31d5d890e7f0fa1cd3e36
OlegStasiv <oleg.stasiv@hotmail.com>
2017-04-28 22:56:43 +0300

    fix minor bug

--- db.py
+++ db.py
@@ -13,12 +13,15 @@ db = firebase.FirebaseApplication('https://yonchi-97f33.firebaseio.com/', authen
 
 def update_result(name, result):
     all_users = db.get("/users", None)
+    user_list = []
     for k, v in all_users.items():
         nickname = v['name'].encode('utf-8')
+        user_list.append(nickname)
         record = v['record']
         if nickname == name:
             if result > record:
                 db.patch('users/' + k, data={"record": result})
                 return True
-    db.post('/users', data={"name": name, "record": result}, params={'print': 'pretty'})
-    return True
+    if name not in user_list:
+        db.post('/users', data={"name": name, "record": result}, params={'print': 'pretty'})
+    
